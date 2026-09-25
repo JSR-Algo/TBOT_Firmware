526794ed62b00b1e2ce529e53eb629374953a22e
Phat Nguyen <phat.nt@hotmail.com>
2026-09-25 15:16:54 +0700

    static PSRAM allow for `lesson_worker` task

--- main/application.cc
+++ main/application.cc
@@ -133,7 +133,7 @@ DRAM_ATTR StackType_t chat_audio_cleanup_task_stack[kChatAudioCleanupWorkerStack
 #if CONFIG_BOARD_TYPE_LCDWIKI_ES3C35P
 DRAM_ATTR StaticTask_t lesson_message_task_buffer;
 DRAM_ATTR StaticQueue_t lesson_message_queue_buffer;
-StackType_t* lesson_message_task_stack = nullptr;
+EXT_RAM_BSS_ATTR StackType_t lesson_message_task_stack[kLessonMessageWorkerStackDepth];
 uint8_t* lesson_message_queue_storage = nullptr;
 
 void LogLessonWorkerStackWatermark(const char* stage) {
@@ -198,17 +198,15 @@ Application::Application() {
 
 #if CONFIG_BOARD_TYPE_LCDWIKI_ES3C35P
     constexpr uint32_t kLessonWorkerMemoryCaps = MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT;
-    lesson_message_task_stack = static_cast<StackType_t*>(heap_caps_malloc(
-        kLessonMessageWorkerStackDepth * sizeof(StackType_t), kLessonWorkerMemoryCaps));
     lesson_message_queue_storage = static_cast<uint8_t*>(heap_caps_malloc(
         (kLessonMessageQueueDepth + 1) * sizeof(LessonQueueItem),
         kLessonWorkerMemoryCaps));
-    if (lesson_message_task_stack != nullptr && lesson_message_queue_storage != nullptr) {
+    if (lesson_message_queue_storage != nullptr) {
         lesson_message_queue_ = xQueueCreateStatic(
             kLessonMessageQueueDepth + 1, sizeof(LessonQueueItem),
             lesson_message_queue_storage, &lesson_message_queue_buffer);
     }
-    if (lesson_message_queue_ != nullptr && lesson_message_task_stack != nullptr) {
+    if (lesson_message_queue_ != nullptr) {
         lesson_message_task_handle_ = xTaskCreateStatic(
             &Application::LessonMessageTask, "lesson_worker", kLessonMessageWorkerStackDepth, this,
             tskIDLE_PRIORITY + 2, lesson_message_task_stack, &lesson_message_task_buffer);
@@ -225,8 +223,6 @@ Application::Application() {
         lesson_message_queue_ = nullptr;
         heap_caps_free(lesson_message_queue_storage);
         lesson_message_queue_storage = nullptr;
-        heap_caps_free(lesson_message_task_stack);
-        lesson_message_task_stack = nullptr;
     }
 #endif
 
@@ -297,8 +293,6 @@ Application::~Application() {
     }
     heap_caps_free(lesson_message_queue_storage);
     lesson_message_queue_storage = nullptr;
-    heap_caps_free(lesson_message_task_stack);
-    lesson_message_task_stack = nullptr;
 #endif
     vEventGroupDelete(event_group_);
 }
